1004a2dfcaf1267a5a96be0cf018c1e6932616b6
absydavsy <abbydavis@outlook.com>
2024-11-25 15:36:35 +0000

    add get topics endpoint

---
+++ api.model.js
@@ -0,0 +1,11 @@
+const db = require("./db/connection")
+
+function selectTopics() {
+    return db.query("SELECT * FROM topics")
+    .then(({rows}) => {
+        return rows
+    })
+
+}
+
+module.exports = selectTopics
--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const endpointsJson = require("./endpoints.json")
-const { getApi } = require("./api.controller")
+const { getApi, getTopics } = require("./api.controller")
 const express = require("express")
 const app = express()
 
+app.use(express.json())
 
 app.get('/api', getApi)
+
+app.get('/api/topics', getTopics)
+
+app.all("*", (req, res) => {
+    res.status(404).send({ msg: "not found"})
+})
 
 module.exports = app;
