436a1ac68eff6b7e2344c9bfcb7d71b095c0b41a
absydavsy <abbydavis@outlook.com>
2024-11-28 16:03:54 +0000

    push changes for hosting

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,20 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+// if (!process.env.PGDATABASE) {
+//   throw new Error('PGDATABASE not set');
+// } (here before, replaced with above from notes for setting up supabase)
+
+
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
